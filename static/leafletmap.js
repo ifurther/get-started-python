7c7207ee6d7e72c5dd13b4c8b42a08cc71f89b61
ifurther <55025025+ifurther@users.noreply.github.com>
2019-11-02 00:27:13 +0800

    MAP

--- static/leafletmap.js
+++ static/leafletmap.js
@@ -1,23 +1,27 @@
 window.onload = function () {
-    var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	});
+	//var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	//	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	//});
+	var map  = L.map('my-map').setView([24.105497, 120.597366], 7);
+	var basemap = L.tileLayer('https://wmts.nlsc.gov.tw/wmts/EMAP/default/GoogleMapsCompatible/{z}/{y}/{x}', {
+        attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	
+    //$.getJSON("census.geojson", function(data) {
 
-    $.getJSON("census.geojson", function(data) {
-
-    var geojson = L.geoJson(data, {
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.Area_Name);
-      }
-    });
+    //var geojson = L.geoJson(data, {
+    //  onEachFeature: function (feature, layer) {
+    //    layer.bindPopup(feature.properties.Area_Name);
+    //  }
+    //});
 
 
-    var map = L.map('my-map')
-    .fitBounds(geojson.getBounds());
-
-    basemap.addTo(map);
-    geojson.addTo(map);
-  });
+    //var map = L.map('my-map')
+    //.fitBounds(geojson.getBounds());
+	
+    //basemap.addTo(map);
+    //geojson.addTo(map);
+  //});
 
 };
 
